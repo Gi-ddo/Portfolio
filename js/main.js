35c60b2477261fcb464b251373aa9d54afabe708
Khethelo-HP-Laptop <saneleknkosi@gmail.com>
2025-05-10 19:02:43 +0200

    update(js): Added Scroll-to-top button logic

--- js/main.js
+++ js/main.js
@@ -1,2 +1,17 @@
-// Future JS scripts (e.g. animations, nav toggle, etc.) go here
-console.log("Portfolio loaded.");
+/// Scroll-to-top button logic
+const scrollBtn = document.getElementById("scrollToTop");
+
+window.addEventListener("scroll", () => {
+  if (window.scrollY > 200) {
+    scrollBtn.style.display = "block";
+  } else {
+    scrollBtn.style.display = "none";
+  }
+});
+
+scrollBtn.addEventListener("click", () => {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth"
+  });
+});
