11cea7aacf3ce83ef9597b9cfdb00ffd73582e3a
Khethelo-HP-Laptop <saneleknkosi@gmail.com>
2025-05-11 14:11:06 +0200

    fix(js): Added placeholder.gif

--- js/project-data.js
+++ js/project-data.js
@@ -2,14 +2,14 @@ const projectData = {
   "fatpack-joe": {
     title: "Fatpack Joe",
     description: "Jetpack chaos meets alien invasion in this Afro-futuristic couch-riding game.",
-    gif: "assets/images/fatpack-joe.gif",
+    gif: "/assets/images/placeholder.gif",
     tech: ["Unity", "C#", "Android"],
     link: "https://play.google.com/store/apps/details?id=fatpack.joe"
   },
   "khamani": {
     title: "Khamani: The Lion of Summer",
     description: "An African fantasy game powered by Unity, filled with puzzles and spirits.",
-    gif: "assets/images/khamani.gif",
+    gif: "/assets/images/placeholder.gif",
     tech: ["Unity", "C#", "PC"],
     link: "#"
   }
