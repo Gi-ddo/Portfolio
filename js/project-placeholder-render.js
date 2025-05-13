6d4d629a2fc03ad2fcac64bb43c4466678683e97
Khethelo Nkosi <saneleknkosi@gmail.com>
2025-05-13 16:20:09 +0200

    update(js): Added js that will render the projects dynamically for the landing page. Will loop through project data

---
+++ js/project-placeholder-render.js
@@ -0,0 +1,23 @@
+import { projectData } from './project-data.js';
+
+const container = document.getElementById("project-list");
+
+if (projectData && container) {
+  Object.entries(projectData).forEach(([id, project]) => {
+    const projectEl = document.createElement("div");
+    projectEl.className = `project-container ${project.alignment}`;
+    
+    projectEl.innerHTML = `
+      <div class="project-text sticky">
+        <h3>${project.title}</h3>
+        <p>${project.short_description}</p>
+        <a href="project.html?id=${id}" class="btn-secondary">View Project</a>
+      </div>
+      <div class="project-media">
+        <img src="${project.gif}" alt="${project.title} Gameplay" />
+      </div>
+    `;
+
+    container.appendChild(projectEl);
+  });
+}
